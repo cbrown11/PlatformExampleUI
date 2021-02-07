6132273190b7fd80520701cc6dee0b1bd1fb0fd0
Craig Brown <craigbrown77@hotmail.com>
2021-02-07 15:39:27 +0000

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ContactsComponent } from './contacts/contacts.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ContactsComponent],
+  imports: [BrowserModule, AppRoutingModule, GraphQLModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
+})
+
+export class ContactsComponent implements OnInit {
+
+  
+  contacts: any[] | undefined;
+  loading = true;
+  error: any;
+  
+  
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .query<any>({
+        query: gql`
+          {
+            contacts {
+              userId
+              firstName
+              lastName
+            }
+          }
+        `
+      })
+      .subscribe(
+        ({ data, loading }) => {
+          this.contacts = data && data.contacts;
+          this.loading = loading;
+        },
+        error => {
+          this.loading = false;
+          this.error = error;
+          console.log(
+            `[GraphQL error]: Message: ${error.Message}`,
+          );
+
+        }
+      );
+}
+
+}
